f9935ce33e0718691507a51c9b16d9881bcee233
yuri0407 <k010682752@gmail.com>
2022-03-22 18:19:05 +0900

    add week03(22.03.22)

---
+++ 3주차/number.py
@@ -0,0 +1,7 @@
+bin(11); bin(0o11); bin(0x11)
+oct(11); oct(0o11); oct(0x11)
+hex(11); hex(0o11); hex(0x11)
+
+print(bin(11), bin(0o11), bin(0x11))
+print(oct(11), oct(0b11), oct(0x11))
+print(hex(11), hex(0b11), hex(0o11))
